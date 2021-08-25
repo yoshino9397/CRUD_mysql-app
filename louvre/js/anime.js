39ad4d0bbdb1b74f989c50de6f9692d98b4c22e3
k-e-r <damajonboua@gmail.com>
2021-08-25 09:20:53 -0700

    add menu bar

--- louvre/js/anime.js
+++ louvre/js/anime.js
@@ -1,7 +1,26 @@
+// menu
 const btn = document.getElementById('btn');
 btn.onclick = btn_event;
 
 function btn_event() {
-  console.log("click");
   btn.classList.toggle('active');
+  dropdown();
+}
+
+const navMobile = document.getElementsByClassName('nav-mobile');
+const navDrop = document.getElementsByClassName('nav-mobile__dropdown-design');
+const navDropSec1 = document.getElementsByClassName('nav-mobile__dropdown-section-1');
+const navDropSec2 = document.getElementsByClassName('nav-mobile__dropdown-section-2');
+const navDropSec3 = document.getElementsByClassName('nav-mobile__dropdown-section-3');
+const bgoverlay = document.getElementsByClassName('bgoverlay');
+
+function dropdown() {
+  // console.log('click');
+  // navMobile.item(0).classList.toggle('active');
+  navDrop.item(0).classList.toggle('active');
+  // navDrop.item(0).classList.add()
+  bgoverlay.item(0).classList.toggle('active');
+  navDropSec1[0].classList.toggle('active');
+  navDropSec2[0].classList.toggle('active');
+  navDropSec3[0].classList.toggle('active');
 }
