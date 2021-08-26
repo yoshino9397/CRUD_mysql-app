1c6551c764dd52f621497843e74c41f0acaa8a3e
k-e-r <damajonboua@gmail.com>
2021-08-25 21:13:13 -0700

    add current exhibitions

--- louvre/js/anime.js
+++ louvre/js/anime.js
@@ -1,3 +1,20 @@
+const exSlides = document.getElementsByClassName('carousel-exhibitions__slide');
+window.addEventListener("load", function() {
+  // const swipeWrapper = document.getElementsByClassName('swipe-wrapper');
+  setSwipe('.swipe-wrapper');
+  exSlides[1].style.opacity = '0.3';
+  exSlides[2].style.opacity = '0.3';
+});
+
+let client_w = document.getElementsByClassName('carousel-exhibitions__slide').item(0).clientWidth;;
+let criterion = document.getElementsByClassName('exhibitions__headline').item(0).getBoundingClientRect();
+window.addEventListener('DOMContentLoaded', function() {
+  window.addEventListener('resize', function() {
+    client_w = document.getElementsByClassName('carousel-exhibitions__slide').item(0).clientWidth;
+    // console.log('client_w:' + client_w);
+  })
+})
+
 // search
 const search = document.getElementById('search');
 search.onclick = search_event;
@@ -5,7 +22,7 @@ const sClose = document.getElementById('search-close');
 sClose.onclick = search_event;
 
 function search_event() {
-  console.log('search');
+  // console.log('search');
   searchView();
 }
 
@@ -56,9 +73,13 @@ const heroHeadline = document.getElementsByClassName('hero__headline');
 const heroEffect = document.getElementsByClassName('hero__infobox__effect');
 const heroWrap = document.getElementsByClassName('hero__wrap');
 
+// chapter heading
+const exHeadline = document.getElementsByClassName('exhibitions__headline');
+
 window.onscroll = () => {
   let scroll_val = window.pageYOffset;
-  console.log(scroll_val);
+  let exHeadline_p = exHeadline[0].getBoundingClientRect();
+  // console.log(scroll_val);
   if (scroll_val <= 200) {
     heroHeadline[0].style.opacity = 1 - (scroll_val/200);
     heroHeadline[0].style.transform = 'translate(0%, ' + (scroll_val/2) +'%)';
@@ -67,10 +88,101 @@ window.onscroll = () => {
   if (scroll_val <= 360) {
     heroEffect[0].style.transform = 'translate(0%, ' + (120 - scroll_val/3) +'%)';
   }
+  
+  // console.log('headline position');
+  // console.log(exHeadline_p.top);
+  if (scroll_val > exHeadline_p.top) {
+    if (exHeadline[0].style.opacity < 1) {
+      exHeadline[0].style.opacity = (scroll_val - exHeadline_p.top) / 500;
+    } else exHeadline[0].style.opacity = 1;
+  }
 }
 
-// .hero__wrap {
-//   position: relative;
-//   padding-top: var(--header-height);
-//   margin-bottom: 100px;
-// }
+const swipeWrapper = document.getElementsByClassName('swipe-wrapper');
+let counter=0;
+function setSwipe(elem) {
+  let t = document.querySelector(elem);
+  let startX, startY;
+  let moveX, moveY;
+  let dist = 50;
+  // let len = document.querySelectorAll(elem + '> a').length;
+  // console.log('length:' + len);
+  let currentStyle = window.getComputedStyle(swipeWrapper[0]);
+  let initVal = currentStyle.getPropertyValue('transform');
+  let initValArray = initVal.split(',');
+
+  t.addEventListener("touchstart", function(e) {
+    if (e.cancelable) e.preventDefault();
+    initVal = currentStyle.getPropertyValue('transform');
+    initValArray = initVal.split(',');
+		startX = e.touches[0].pageX;
+		startY = e.touches[0].pageY;
+    // console.log('startX:'+startX);
+    // console.log('startY:'+startY);
+    // console.log('client_w:'+client_w);
+    // console.log('initVal:' + currentStyle.getPropertyValue('transform'));
+    // console.log('initVal:' + initValArray[4]);
+	});
+
+  t.addEventListener("touchmove", function(e) {
+    if (e.cancelable) e.preventDefault();
+		moveX = e.changedTouches[0].pageX;
+		moveY = e.changedTouches[0].pageY;
+    // console.log('moveX:'+moveX);
+    // console.log('moveY:'+moveY);
+    if ((startX>moveX) && (startX>moveX)) {
+      let movingVal = startX-moveX;
+      if (counter === 0) {
+        swipeWrapper[0].style.transform = 'translate3d(-' + (movingVal) +'px, 0px, 0px)';
+      } else if (counter === 1) {
+        swipeWrapper[0].style.transform = 'translate3d(-' + (client_w+movingVal) +'px, 0px, 0px)';
+      }
+      console.log(movingVal);
+    } else if ((startX<moveX) && (startX<moveX)) {
+      let movingVal = moveX-startX;
+      // from left to right
+      if (counter === 2) {
+        swipeWrapper[0].style.transform = 'translate3d(-' + (client_w*2-movingVal) +'px, 0px, 0px)';
+      } else if (counter === 1) {
+        swipeWrapper[0].style.transform = 'translate3d(-' + (client_w-movingVal) + 'px, 0px, 0px)';
+      }
+      // console.log('right');
+    }
+	});
+
+  t.addEventListener("touchend", function(e) {
+    if ((startX>moveX) && (startX>(moveX+dist))) {
+      // from right to left
+      if (counter === 0) {
+        swipeWrapper[0].style.transform = 'translate3d(-' + client_w +'px, 0px, 0px)';
+        exSlides[0].style.opacity = '0.3';
+        exSlides[1].style.opacity = '1';
+        counter++;
+      } else if (counter === 1) {
+        swipeWrapper[0].style.transform = 'translate3d(-' + client_w*2 +'px, 0px, 0px)';
+        exSlides[1].style.opacity = '0.3';
+        exSlides[2].style.opacity = '1';
+        counter++;
+      }
+      // console.log('left');
+    } else if ((startX<moveX) && ((startX+dist)<moveX)) {
+      // from left to right
+      if (counter === 2) {
+        swipeWrapper[0].style.transform = 'translate3d(-' + client_w +'px, 0px, 0px)';
+        exSlides[1].style.opacity = '1';
+        exSlides[2].style.opacity = '0.3';
+        counter--;
+      } else if (counter === 1) {
+        swipeWrapper[0].style.transform = 'translate3d(0px, 0px, 0px)';
+        exSlides[0].style.opacity = '1';
+        exSlides[1].style.opacity = '0.3';
+        counter--;
+      }
+      // console.log('right');
+    } else {
+      swipeWrapper[0].style.transform = 'translate3d(' + initValArray[4] +'px, 0px, 0px)';
+    }
+  });
+}
+
+// style="transform: translate3d(0px, 0px, 0px);
