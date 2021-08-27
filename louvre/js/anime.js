d297b5138dc1216a8efdc2fe192145d26625266e
k-e-r <damajonboua@gmail.com>
2021-08-26 19:06:49 -0700

    add carousel function

--- louvre/js/anime.js
+++ louvre/js/anime.js
@@ -1,7 +1,7 @@
 const exSlides = document.getElementsByClassName('carousel-exhibitions__slide');
 window.addEventListener("load", function() {
-  // const swipeWrapper = document.getElementsByClassName('swipe-wrapper');
-  setSwipe('.swipe-wrapper');
+  // setSwipe('.swipe-wrapper');
+  setSwipe('.swipe-container')
   exSlides[1].style.opacity = '0.3';
   exSlides[2].style.opacity = '0.3';
 });
@@ -76,6 +76,12 @@ const heroWrap = document.getElementsByClassName('hero__wrap');
 // chapter heading
 const exHeadline = document.getElementsByClassName('exhibitions__headline');
 
+// shadow
+const spotLazyload = document.getElementsByClassName('spot-lazyload');
+const spotShadow = document.getElementsByClassName('spot-shadow');
+const spot_p = spotLazyload[0].getBoundingClientRect();
+let flag = 0;
+
 window.onscroll = () => {
   let scroll_val = window.pageYOffset;
   let exHeadline_p = exHeadline[0].getBoundingClientRect();
@@ -96,62 +102,108 @@ window.onscroll = () => {
       exHeadline[0].style.opacity = (scroll_val - exHeadline_p.top) / 500;
     } else exHeadline[0].style.opacity = 1;
   }
+
+  // console.log(scroll_val);
+  // console.log(spot_p.top);
+  if (scroll_val > (spot_p.top)) {
+    if (flag === 0) {
+      spotShadow[0].classList.toggle('active');
+      flag++;
+    }
+  }
+  if (scroll_val > (spot_p.top+200)) {
+    if (flag === 1) {
+      spotLazyload[0].classList.toggle('active');
+      flag++;
+    }
+  }
 }
 
-const swipeWrapper = document.getElementsByClassName('swipe-wrapper');
+spotLazyload[0].addEventListener('mouseover', function() {
+  mouseOver();
+}, false);
+spotLazyload[0].addEventListener('mouseleave', function() {
+  mouseLeave();
+}, false);
+
+// マウスが要素上に入った時
+function mouseOver () {
+  console.log('over');
+  // spotLazyload[0].style.opacity = '0';
+}
+
+// マウスが要素上から離れた時
+function mouseLeave () {
+  console.log('leave');
+  // spotLazyload[0].style.opacity = '1';
+}
+
+
+
+
+
+let swipeWrapper = document.getElementsByClassName('swipe-wrapper');
 let counter=0;
 function setSwipe(elem) {
   let t = document.querySelector(elem);
   let startX, startY;
   let moveX, moveY;
-  let dist = 50;
-  // let len = document.querySelectorAll(elem + '> a').length;
-  // console.log('length:' + len);
+  let dist = 100, dist2 = 30;
+  let len = document.querySelectorAll(elem + '> a').length;
+  console.log('length:' + len);
   let currentStyle = window.getComputedStyle(swipeWrapper[0]);
   let initVal = currentStyle.getPropertyValue('transform');
   let initValArray = initVal.split(',');
+  let flag=0, stop_jump=0;
 
-  t.addEventListener("touchstart", function(e) {
-    if (e.cancelable) e.preventDefault();
+  t.addEventListener("mousedown", function(e) {
+    e.preventDefault();
     initVal = currentStyle.getPropertyValue('transform');
     initValArray = initVal.split(',');
-		startX = e.touches[0].pageX;
-		startY = e.touches[0].pageY;
-    // console.log('startX:'+startX);
-    // console.log('startY:'+startY);
-    // console.log('client_w:'+client_w);
-    // console.log('initVal:' + currentStyle.getPropertyValue('transform'));
-    // console.log('initVal:' + initValArray[4]);
-	});
+    startX = e.x;
+    startY = e.y;
+    console.log('startX:'+startX);
+    console.log('startY:'+startY);
+    console.log('client_w:'+client_w);
+    console.log('initVal:' + currentStyle.getPropertyValue('transform'));
+    console.log('initVal:' + initValArray[4]);
+    flag=1;
+  }, { passive: false });
 
-  t.addEventListener("touchmove", function(e) {
-    if (e.cancelable) e.preventDefault();
-		moveX = e.changedTouches[0].pageX;
-		moveY = e.changedTouches[0].pageY;
-    // console.log('moveX:'+moveX);
-    // console.log('moveY:'+moveY);
-    if ((startX>moveX) && (startX>moveX)) {
-      let movingVal = startX-moveX;
-      if (counter === 0) {
-        swipeWrapper[0].style.transform = 'translate3d(-' + (movingVal) +'px, 0px, 0px)';
-      } else if (counter === 1) {
-        swipeWrapper[0].style.transform = 'translate3d(-' + (client_w+movingVal) +'px, 0px, 0px)';
+  t.addEventListener("mousemove", function(e) {
+    e.preventDefault();
+    if (flag === 1) {
+      moveX = e.x;
+      moveY = e.y;
+      console.log('moveX:'+moveX);
+      console.log('moveY:'+moveY);
+
+      if (startX>(moveX+dist2)) {
+        stop_jump=1;
+        let movingVal = startX-moveX;
+        if (counter === 0) {
+          swipeWrapper[0].style.transform = 'translate3d(-' + (movingVal) +'px, 0px, 0px)';
+        } else if (counter === 1) {
+          swipeWrapper[0].style.transform = 'translate3d(-' + (client_w+movingVal) +'px, 0px, 0px)';
+        }
+        console.log(movingVal);
+      } else if ((startX+dist2)<moveX) {
+        stop_jump=1;
+        let movingVal = moveX-startX;
+        // from left to right
+        if (counter === 2) {
+          swipeWrapper[0].style.transform = 'translate3d(-' + (client_w*2-movingVal) +'px, 0px, 0px)';
+        } else if (counter === 1) {
+          swipeWrapper[0].style.transform = 'translate3d(-' + (client_w-movingVal) + 'px, 0px, 0px)';
+        }
+        console.log('right');
       }
-      console.log(movingVal);
-    } else if ((startX<moveX) && (startX<moveX)) {
-      let movingVal = moveX-startX;
-      // from left to right
-      if (counter === 2) {
-        swipeWrapper[0].style.transform = 'translate3d(-' + (client_w*2-movingVal) +'px, 0px, 0px)';
-      } else if (counter === 1) {
-        swipeWrapper[0].style.transform = 'translate3d(-' + (client_w-movingVal) + 'px, 0px, 0px)';
-      }
-      // console.log('right');
     }
-	});
+  }, { passive: false });
 
-  t.addEventListener("touchend", function(e) {
-    if ((startX>moveX) && (startX>(moveX+dist))) {
+  t.addEventListener("mouseup", function(e) {
+    e.preventDefault();
+    if (startX>(moveX+dist)) {
       // from right to left
       if (counter === 0) {
         swipeWrapper[0].style.transform = 'translate3d(-' + client_w +'px, 0px, 0px)';
@@ -164,8 +216,8 @@ function setSwipe(elem) {
         exSlides[2].style.opacity = '1';
         counter++;
       }
-      // console.log('left');
-    } else if ((startX<moveX) && ((startX+dist)<moveX)) {
+      console.log('left');
+    } else if ((startX+dist)<moveX) {
       // from left to right
       if (counter === 2) {
         swipeWrapper[0].style.transform = 'translate3d(-' + client_w +'px, 0px, 0px)';
@@ -178,11 +230,185 @@ function setSwipe(elem) {
         exSlides[1].style.opacity = '0.3';
         counter--;
       }
-      // console.log('right');
+      console.log('right');
     } else {
       swipeWrapper[0].style.transform = 'translate3d(' + initValArray[4] +'px, 0px, 0px)';
     }
-  });
+    flag=0;
+  }, { passive: false });
 }
+
+
+// swipeWrapper[0].onmousedown = function(e) {
+//   console.log('moveX:' + e.pageX + '/Y:' + e.pageY);
+//   let t = document.querySelector(elem);
+//   let startX, startY;
+//   let moveX, moveY;
+//   let dist = 50;
+//   let len = document.querySelectorAll(elem + '> a').length;
+//   console.log('length:' + len);
+//   let currentStyle = window.getComputedStyle(swipeWrapper[0]);
+//   let initVal = currentStyle.getPropertyValue('transform');
+//   let initValArray = initVal.split(',');
+
+//   t.addEventListener("touchstart", function(e) {
+//     if (e.cancelable) e.preventDefault();
+//     initVal = currentStyle.getPropertyValue('transform');
+//     initValArray = initVal.split(',');
+// 		startX = e.touches[0].pageX;
+// 		startY = e.touches[0].pageY;
+//     console.log('startX:'+startX);
+//     console.log('startY:'+startY);
+//     console.log('client_w:'+client_w);
+//     console.log('initVal:' + currentStyle.getPropertyValue('transform'));
+//     console.log('initVal:' + initValArray[4]);
+// 	});
+
+//   t.addEventListener("touchmove", function(e) {
+//     if (e.cancelable) e.preventDefault();
+// 		moveX = e.changedTouches[0].pageX;
+// 		moveY = e.changedTouches[0].pageY;
+//     console.log('moveX:'+moveX);
+//     console.log('moveY:'+moveY);
+//     if (startX>moveX) {
+//       let movingVal = startX-moveX;
+//       if (counter === 0) {
+//         swipeWrapper[0].style.transform = 'translate3d(-' + (movingVal) +'px, 0px, 0px)';
+//       } else if (counter === 1) {
+//         swipeWrapper[0].style.transform = 'translate3d(-' + (client_w+movingVal) +'px, 0px, 0px)';
+//       }
+//       console.log(movingVal);
+//     } else if (startX<moveX) {
+//       let movingVal = moveX-startX;
+//       // from left to right
+//       if (counter === 2) {
+//         swipeWrapper[0].style.transform = 'translate3d(-' + (client_w*2-movingVal) +'px, 0px, 0px)';
+//       } else if (counter === 1) {
+//         swipeWrapper[0].style.transform = 'translate3d(-' + (client_w-movingVal) + 'px, 0px, 0px)';
+//       }
+//       console.log('right');
+//     }
+// 	});
+
+//   t.addEventListener("touchend", function(e) {
+//     if ((startX>moveX) && (startX>(moveX+dist))) {
+//       // from right to left
+//       if (counter === 0) {
+//         swipeWrapper[0].style.transform = 'translate3d(-' + client_w +'px, 0px, 0px)';
+//         exSlides[0].style.opacity = '0.3';
+//         exSlides[1].style.opacity = '1';
+//         counter++;
+//       } else if (counter === 1) {
+//         swipeWrapper[0].style.transform = 'translate3d(-' + client_w*2 +'px, 0px, 0px)';
+//         exSlides[1].style.opacity = '0.3';
+//         exSlides[2].style.opacity = '1';
+//         counter++;
+//       }
+//       console.log('left');
+//     } else if ((startX<moveX) && ((startX+dist)<moveX)) {
+//       // from left to right
+//       if (counter === 2) {
+//         swipeWrapper[0].style.transform = 'translate3d(-' + client_w +'px, 0px, 0px)';
+//         exSlides[1].style.opacity = '1';
+//         exSlides[2].style.opacity = '0.3';
+//         counter--;
+//       } else if (counter === 1) {
+//         swipeWrapper[0].style.transform = 'translate3d(0px, 0px, 0px)';
+//         exSlides[0].style.opacity = '1';
+//         exSlides[1].style.opacity = '0.3';
+//         counter--;
+//       }
+//       console.log('right');
+//     } else {
+//       swipeWrapper[0].style.transform = 'translate3d(' + initValArray[4] +'px, 0px, 0px)';
+//     }
+//   });
+// };
+
+// let counter=0;
+// function setSwipe(elem) {
+//   let t = document.querySelector(elem);
+//   let startX, startY;
+//   let moveX, moveY;
+//   let dist = 50;
+//   let len = document.querySelectorAll(elem + '> a').length;
+//   console.log('length:' + len);
+//   let currentStyle = window.getComputedStyle(swipeWrapper[0]);
+//   let initVal = currentStyle.getPropertyValue('transform');
+//   let initValArray = initVal.split(',');
+
+//   t.addEventListener("touchstart", function(e) {
+//     if (e.cancelable) e.preventDefault();
+//     initVal = currentStyle.getPropertyValue('transform');
+//     initValArray = initVal.split(',');
+// 		startX = e.touches[0].pageX;
+// 		startY = e.touches[0].pageY;
+//     console.log('startX:'+startX);
+//     console.log('startY:'+startY);
+//     console.log('client_w:'+client_w);
+//     console.log('initVal:' + currentStyle.getPropertyValue('transform'));
+//     console.log('initVal:' + initValArray[4]);
+// 	});
+
+//   t.addEventListener("touchmove", function(e) {
+//     if (e.cancelable) e.preventDefault();
+// 		moveX = e.changedTouches[0].pageX;
+// 		moveY = e.changedTouches[0].pageY;
+//     console.log('moveX:'+moveX);
+//     console.log('moveY:'+moveY);
+//     if ((startX>moveX) && (startX>moveX)) {
+//       let movingVal = startX-moveX;
+//       if (counter === 0) {
+//         swipeWrapper[0].style.transform = 'translate3d(-' + (movingVal) +'px, 0px, 0px)';
+//       } else if (counter === 1) {
+//         swipeWrapper[0].style.transform = 'translate3d(-' + (client_w+movingVal) +'px, 0px, 0px)';
+//       }
+//       console.log(movingVal);
+//     } else if ((startX<moveX) && (startX<moveX)) {
+//       let movingVal = moveX-startX;
+//       // from left to right
+//       if (counter === 2) {
+//         swipeWrapper[0].style.transform = 'translate3d(-' + (client_w*2-movingVal) +'px, 0px, 0px)';
+//       } else if (counter === 1) {
+//         swipeWrapper[0].style.transform = 'translate3d(-' + (client_w-movingVal) + 'px, 0px, 0px)';
+//       }
+//       console.log('right');
+//     }
+// 	});
+
+//   t.addEventListener("touchend", function(e) {
+//     if ((startX>moveX) && (startX>(moveX+dist))) {
+//       // from right to left
+//       if (counter === 0) {
+//         swipeWrapper[0].style.transform = 'translate3d(-' + client_w +'px, 0px, 0px)';
+//         exSlides[0].style.opacity = '0.3';
+//         exSlides[1].style.opacity = '1';
+//         counter++;
+//       } else if (counter === 1) {
+//         swipeWrapper[0].style.transform = 'translate3d(-' + client_w*2 +'px, 0px, 0px)';
+//         exSlides[1].style.opacity = '0.3';
+//         exSlides[2].style.opacity = '1';
+//         counter++;
+//       }
+//       console.log('left');
+//     } else if ((startX<moveX) && ((startX+dist)<moveX)) {
+//       // from left to right
+//       if (counter === 2) {
+//         swipeWrapper[0].style.transform = 'translate3d(-' + client_w +'px, 0px, 0px)';
+//         exSlides[1].style.opacity = '1';
+//         exSlides[2].style.opacity = '0.3';
+//         counter--;
+//       } else if (counter === 1) {
+//         swipeWrapper[0].style.transform = 'translate3d(0px, 0px, 0px)';
+//         exSlides[0].style.opacity = '1';
+//         exSlides[1].style.opacity = '0.3';
+//         counter--;
+//       }
+//       console.log('right');
+//     } else {
+//       swipeWrapper[0].style.transform = 'translate3d(' + initValArray[4] +'px, 0px, 0px)';
+//     }
+//   });
+// }
 
 // style="transform: translate3d(0px, 0px, 0px);
